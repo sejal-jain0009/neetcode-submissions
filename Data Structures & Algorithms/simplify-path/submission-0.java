f2faf0f4112a2679a9baf3c97c63f2f88c360b74
Sejal <sejaljain0009@gmail.com>
2026-07-25 23:54:38 +0530

    Add: simplify-path - submission-0

---
+++ Data Structures & Algorithms/simplify-path/submission-0.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String simplifyPath(String path) {
+
+        Stack<String> st = new Stack<>();
+
+        String[] arr = path.split("/");
+
+        for (String s : arr) {
+
+            if (s.equals("") || s.equals(".")) {
+                continue;
+            }
+
+            else if (s.equals("..")) {
+                if (!st.isEmpty()) {
+                    st.pop();
+                }
+            }
+
+            else {
+                st.push(s);
+            }
+        }
+
+        if (st.isEmpty()) {
+            return "/";
+        }
+
+        StringBuilder ans = new StringBuilder();
+
+        for (String dir : st) {
+            ans.append("/").append(dir);
+        }
+
+        return ans.toString();
+    }
+}
